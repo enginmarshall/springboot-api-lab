0dd61401d98bf61929417cf770ac429836a382c9
engin.marshall <engin.marshall@gmail.com>
2024-10-16 22:03:11 +0200

    Added demo controller

--- src/test/java/com/example/api_lab/api_lab/ApiLabApplicationTests.java
+++ src/test/java/com/example/api_lab/api_lab/ApiLabApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.api_lab.api_lab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PayrollApplicationTests {
+class ApiLabApplicationTests {
 
 	@Test
 	void contextLoads() {
